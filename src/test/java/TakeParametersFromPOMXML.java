c6a72073499b1d0c9790992304f46583dfc6447e
shashank <shashank.deepak@gmail.com>
2022-01-15 21:31:29 -0600

    changes1

--- src/test/java/TakeParametersFromPOMXML.java
+++ src/test/java/TakeParametersFromPOMXML.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 
 public class TakeParametersFromPOMXML {
 
-    @Parameters("browser")   //take same name as mentioned in POM.XML
+    @Parameters("browser")   //take same name as mentioned in POM.XML TagName
     @Test
     void takingParameterFromPOM(String myBrowser){
         System.out.println("parameter  is "+ myBrowser);
