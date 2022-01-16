333cd9595c7a8dd091f21630cd0bf5c43a9fa1ff
shashank <shashank.deepak@gmail.com>
2022-01-15 21:45:26 -0600

    changes1

--- src/test/java/TakeParametersFromPOMXML.java
+++ src/test/java/TakeParametersFromPOMXML.java
@@ -3,11 +3,24 @@ import org.testng.annotations.Test;
 
 public class TakeParametersFromPOMXML {
 
-    @Parameters({"browser","clientname"})   //take same name as mentioned in POM.XML TagName
+//    @Parameters({"browser","clientname"})   //take same name as mentioned in POM.XML TagName
+//    @Test
+//    void takingParameterFromPOM(String myBrowser, String myClientName){
+//        System.out.println("parameter browser is "+ myBrowser);
+//        System.out.println("parameter clntnm is "+ myClientName);
+//
+//        if(myBrowser.equalsIgnoreCase("Chrome")){
+//            System.out.println("i am in chrome");
+//        }
+//        else if(myBrowser.equalsIgnoreCase("ie")){
+//            System.out.println("i am in ie");
+//        }
+//
+//    }
+    @Parameters({"browser"})   //take same name as mentioned in POM.XML TagName
     @Test
-    void takingParameterFromPOM(String myBrowser, String myClientName){
+    void takingParameterFromPOM1(String myBrowser){
         System.out.println("parameter browser is "+ myBrowser);
-        System.out.println("parameter clntnm is "+ myClientName);
 
         if(myBrowser.equalsIgnoreCase("Chrome")){
             System.out.println("i am in chrome");
