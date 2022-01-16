f1920083a8c32a76a35148e3bddcf15270a7c38e
shashank <shashank.deepak@gmail.com>
2022-01-15 21:27:32 -0600

    changes1

---
+++ src/test/java/TakeParametersFromPOMXML.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TakeParametersFromPOMXML {
+
+    @Parameters("browser")   //take same name as mentioned in POM.XML
+    @Test
+    void takingParameterFromPOM(String myBrowser){
+        System.out.println("parameter  is "+ myBrowser);
+
+        if(myBrowser.equalsIgnoreCase("Chrome")){
+            System.out.println("i am in chrome");
+        }
+        else if(myBrowser.equalsIgnoreCase("ie")){
+            System.out.println("i am in ie");
+        }
+
+    }
+}
